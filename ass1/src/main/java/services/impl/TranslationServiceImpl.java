de1e51ce06bae94c8465befdf4ba7d83d7b87dc7
alexlaggner <allaggner@edu.aau.at>
2023-04-25 21:09:00 +0200

    translationservicetest

--- ass1/src/main/java/services/impl/TranslationServiceImpl.java
+++ ass1/src/main/java/services/impl/TranslationServiceImpl.java
@@ -9,6 +9,9 @@ import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 import services.interfaces.TranslationService;
 
@@ -21,22 +24,26 @@ import java.util.List;
 @Service
 public class TranslationServiceImpl implements TranslationService {
     private static final String API_ENDPOINT = "google-translator9.p.rapidapi.com";
-    private static final String API_KEY="f5d9689c49msh52e52af4cc1a8c1p1125dbjsn7963f5a59953";
-    private static final String URL ="https://google-translator9.p.rapidapi.com/v2";
+    private static final String API_KEY = "f5d9689c49msh52e52af4cc1a8c1p1125dbjsn7963f5a59953";
+    private static final String URL = "https://google-translator9.p.rapidapi.com/v2";
     private HttpClient httpClient;
 
     public List<String> translateHeadings(List<String> headings, Language language) {
         List<String> translatedList = new LinkedList<String>();
         try {
-            if(headings != null){
-            for (String heading : headings) {
-                HttpPost httpPost = preparePostRequest(heading, language);
+            if (headings != null) {
+                for (String heading : headings) {
+                    HttpPost httpPost = preparePostRequest(heading, language);
 
-                HttpResponse response = httpClient.execute(httpPost);
-                HttpEntity responseEntity = response.getEntity();
-
-                translatedList.add(EntityUtils.toString(responseEntity));
-            }
+                    HttpResponse response = httpClient.execute(httpPost);
+                    HttpEntity responseEntity = response.getEntity();
+                    try {
+                        String translatedTextFromJson = extractTranslatedTextFromJson(EntityUtils.toString(responseEntity));
+                        translatedList.add(translatedTextFromJson);
+                    } catch (JSONException e) {
+                        translatedList.add(EntityUtils.toString(responseEntity));
+                    }
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -44,18 +51,25 @@ public class TranslationServiceImpl implements TranslationService {
         return translatedList;
     }
 
-    private HttpPost preparePostRequest(String text, Language language){
+    private HttpPost preparePostRequest(String text, Language language) {
         HttpPost httpPost = new HttpPost(URL);
         httpPost.setHeader("Content-type", "application/json");
         httpPost.setHeader("X-RapidAPI-Key", API_KEY);
         httpPost.setHeader("X-RapidAPI-Host", API_ENDPOINT);
 
-        StringEntity entity = new StringEntity("{\"q\":\""+text+"\",\"target\":\""+language.getCode()+"\",\"format\":\"text\"}",
+        StringEntity entity = new StringEntity("{\"q\":\"" + text + "\",\"target\":\"" + language.getCode() + "\",\"format\":\"text\"}",
                 ContentType.APPLICATION_JSON);
 
         httpPost.setEntity(entity);
 
         return httpPost;
+    }
+
+    private String extractTranslatedTextFromJson(String json) throws JSONException {
+        JSONObject jsonObj = new JSONObject(json);
+        JSONArray translationsArr = jsonObj.getJSONObject("data").getJSONArray("translations");
+        String translatedText = translationsArr.getJSONObject(0).getString("translatedText");
+        return translatedText;
     }
 
     @PostConstruct
--- ass1/src/test/java/TranslationServiceTests.java
+++ ass1/src/test/java/TranslationServiceTests.java
@@ -1,2 +1,126 @@
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import models.enumerations.Language;
+import services.impl.TranslationServiceImpl;
+import services.interfaces.TranslationService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = TranslationServiceImpl.class)
 public class TranslationServiceTests {
+
+    @MockBean
+    private HttpClient httpClient;
+    @MockBean
+    private List<HttpResponse> responses;
+
+    @Autowired
+    private TranslationService translationService;
+
+    private List<HttpPost> httpPosts = new LinkedList<>();
+    private List<String> responseString = new LinkedList<>();
+
+    private List<String> headings;
+    private Language language;
+
+    private static final String API_ENDPOINT = "google-translator9.p.rapidapi.com";
+    private static final String API_KEY="f5d9689c49msh52e52af4cc1a8c1p1125dbjsn7963f5a59953";
+    private static final String URL ="https://google-translator9.p.rapidapi.com/v2";
+
+    HttpResponse response1 = Mockito.mock(HttpResponse.class);
+    HttpResponse response2 = Mockito.mock(HttpResponse.class);
+    HttpResponse response3 = Mockito.mock(HttpResponse.class);
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        String [] headings = {"Header1", "Header2", "Header3"};
+        List<String> headingList = Arrays.asList(headings);
+        this.headings = headingList;
+        this.language = Language.EN;
+
+
+        for (String text : headingList) {
+            HttpPost httpPost = new HttpPost(URL);
+            httpPost.setHeader("Content-type", "application/json");
+            httpPost.setHeader("X-RapidAPI-Key", API_KEY);
+            httpPost.setHeader("X-RapidAPI-Host", API_ENDPOINT);
+
+            StringEntity entity = new StringEntity("{\"q\":\"" + text + "\",\"target\":\"" + language.getCode() + "\",\"format\":\"text\"}",
+                    ContentType.APPLICATION_JSON);
+
+            httpPost.setEntity(entity);
+            this.httpPosts.add(httpPost);
+            String firstResponseString ="{\n" +
+                    "  \"data\": {\n" +
+                    "    \"translations\": [\n" +
+                    "      {\n" +
+                    "        \"translatedText\": \"Header1\",\n" +
+                    "        \"detectedSourceLanguage\": \"de\"\n" +
+                    "      }\n" +
+                    "    ]\n" +
+                    "  }\n" +
+                    "}";
+            String secondResponseString ="{\n" +
+                    "  \"data\": {\n" +
+                    "    \"translations\": [\n" +
+                    "      {\n" +
+                    "        \"translatedText\": \"Header2\",\n" +
+                    "        \"detectedSourceLanguage\": \"de\"\n" +
+                    "      }\n" +
+                    "    ]\n" +
+                    "  }\n" +
+                    "}";
+            String thirdResponseString ="{\n" +
+                    "  \"data\": {\n" +
+                    "    \"translations\": [\n" +
+                    "      {\n" +
+                    "        \"translatedText\": \"Header3\",\n" +
+                    "        \"detectedSourceLanguage\": \"de\"\n" +
+                    "      }\n" +
+                    "    ]\n" +
+                    "  }\n" +
+                    "}";
+            this.responseString.add(firstResponseString);
+            this.responseString.add(secondResponseString);
+            this.responseString.add(thirdResponseString);
+
+        }
+    }
+
+    @Test
+    public void testTranslation() throws Exception {
+        Mockito.when(httpClient.execute(httpPosts.get(0))).thenReturn((response1));
+        Mockito.when(response1.getEntity()).thenReturn(new StringEntity(responseString.get(0)));
+
+        Mockito.when(httpClient.execute(httpPosts.get(1))).thenReturn((response2));
+        Mockito.when(response2.getEntity()).thenReturn(new StringEntity(responseString.get(1)));
+
+        Mockito.when(httpClient.execute(httpPosts.get(2))).thenReturn((response3));
+        Mockito.when(response3.getEntity()).thenReturn(new StringEntity(responseString.get(2)));
+
+        translationService.translateHeadings(headings,language);
+        Assert.assertEquals("Header1", headings.get(0));
+        Assert.assertEquals("Header2", headings.get(1));
+        Assert.assertEquals("Header3", headings.get(2));
+
+    }
 }
